eb5d7201be9e205b20d2a8cd9ad196a73e395491
vsckawa <109258421+hkreds@users.noreply.github.com>
2023-01-15 15:17:58 +0900

    [Finish]練習問題7

---
+++ src/java_practice/CoinCase.java
@@ -0,0 +1,60 @@
+package java_practice;
+
+public class CoinCase {
+	private int[] Counts = new int[6];
+	public void AddCoins(int kind, int count) {
+		if(kind==500) {
+			Counts[0] += count;
+		}else if (kind==100) {
+			Counts[1] += count;
+		}else if (kind==50) {
+			Counts[2] += count;
+		}else if (kind==10) {
+			Counts[3] += count;
+		}else if (kind==5) {
+			Counts[4] += count;
+		}else if (kind==1) {
+			Counts[5] += count;
+		}
+	}
+	public int GetCount(int kind) {
+		if(kind==500) {
+			return Counts[0];
+		}else if (kind==100) {
+			return Counts[1];
+		}else if (kind==50) {
+			return Counts[2];
+		}else if (kind==10) {
+			return Counts[3];
+		}else if (kind==5) {
+			return Counts[4];
+		}else if (kind==1) {
+			return Counts[5];
+		}else {
+			return 0;
+		}
+	}
+	public int GetAmount() {
+		return (500*Counts[0])+(100*Counts[1])+(50*Counts[2])+(10*Counts[3])+(5*Counts[4])+(1*Counts[5]);
+	}
+	public int AllGetCount() {
+		return Counts[0]+Counts[1]+Counts[2]+Counts[3]+Counts[4]+Counts[5];
+	}
+	public int EachGetAmount(int kind) {
+		if(kind==500) {
+			return Counts[0]*500;
+		}else if (kind==100) {
+			return Counts[1]*100;
+		}else if (kind==50) {
+			return Counts[2]*50;
+		}else if (kind==10) {
+			return Counts[3]*10;
+		}else if (kind==5) {
+			return Counts[4]*5;
+		}else if (kind==1) {
+			return Counts[5]*1;
+		}else {
+			return 0;
+		}
+	}
+}
--- src/java_practice/Practice7.java
+++ src/java_practice/Practice7.java
@@ -1,7 +1,12 @@
 package java_practice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Practice7 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader( System.in ) );
 		System.out.println("練習問題7-1");
 		Dog dog = new Dog("柴犬");
 		dog.SetName("犬");
@@ -26,5 +31,40 @@ public class Practice7 {
 		dog3.SetName("サブロー");
 		dog3.SetAge(4);
 		dog3.ShowProfile();
+
+//		System.out.println("練習問題7-5");
+//		CoinCase case5 = new CoinCase();
+//		for (int i= 0;i<=10;i++) {
+//			System.out.print( "硬貨の種類は?" );
+//      int kind = Integer.parseInt( br.readLine() );
+//      System.out.print( "硬貨の枚数は?" );
+//      int count = Integer.parseInt( br.readLine() );
+//      case5.AddCoins(kind,count);
+//		}
+//		System.out.println("500円："+case5.GetCount(500)+"枚");
+//		System.out.println("100円："+case5.GetCount(100)+"枚");
+//		System.out.println("50円："+case5.GetCount(50)+"枚");
+//		System.out.println("10円："+case5.GetCount(10)+"枚");
+//		System.out.println("5円："+case5.GetCount(5)+"枚");
+//		System.out.println("1円："+case5.GetCount(1)+"枚");
+//		System.out.println("総額:"+case5.GetAmount()+"円");
+
+		System.out.println("練習問題7-6");
+		CoinCase case6 = new CoinCase();
+		for (int i= 0;i<=5;i++) {
+			System.out.print( "硬貨の種類は?" );
+      int kind = Integer.parseInt( br.readLine() );
+      System.out.print( "硬貨の枚数は?" );
+      int count = Integer.parseInt( br.readLine() );
+      case6.AddCoins(kind,count);
+		}
+		System.out.println("500円："+case6.GetCount(500)+"枚、"+case6.EachGetAmount(500)+"円");
+		System.out.println("100円："+case6.GetCount(100)+"枚、"+case6.EachGetAmount(100)+"円");
+		System.out.println("50円："+case6.GetCount(50)+"枚、"+case6.EachGetAmount(50)+"円");
+		System.out.println("10円："+case6.GetCount(10)+"枚、"+case6.EachGetAmount(10)+"円");
+		System.out.println("5円："+case6.GetCount(5)+"枚、"+case6.EachGetAmount(5)+"円");
+		System.out.println("1円："+case6.GetCount(1)+"枚、"+case6.EachGetAmount(1)+"円");
+		System.out.println(case6.AllGetCount());
+		System.out.println("総額:"+case6.GetAmount()+"円");
 	}
 }
