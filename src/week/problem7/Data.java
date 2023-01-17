a96aa85145cf8c5dc08a1927e8e3633d8ae0d7ea
vsckawa <109258421+hkreds@users.noreply.github.com>
2023-01-17 14:19:02 +0900

    [Finish]問題7

---
+++ src/week/problem7/Data.java
@@ -0,0 +1,18 @@
+package week.problem7;
+
+public class Data {
+	private int number = 0;
+  private String comment = "";
+  public void setNumber(int number) {
+		this.number = number;
+	}
+  public void setComment(String comment) {
+		this.comment = comment;
+  }
+  public int getNumber() {
+		return this.number;
+	}
+  public String getComment() {
+		return this.comment;
+	}
+}
---
+++ src/week/problem7/Problem7_3.java
@@ -0,0 +1,12 @@
+package week.problem7;
+
+public class Problem7_3 {
+	public static void main(String[] args) {
+		Calculation c = new Calculation();
+		Calculation cc = new Calculation();
+    c.setNumber1(8);
+    c.setNumber2(9);
+    System.out.println(c.getNumber1() + " + " + c.getNumber2() + " = " + c.add());
+    System.out.println(c.getNumber1() + " - " + c.getNumber2() + " = " + c.sub());
+	}
+}
